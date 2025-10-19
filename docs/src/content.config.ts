df88b33c44bcf74e3558b2ebd27879ad8e6151bf
Hayden Hung Hoang <hayden.hhoang@gmail.com>
2025-10-19 21:20:18 +0700

    i18n docsearch vietnamese

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,7 +1,12 @@
-import { defineCollection } from 'astro:content'
-import { docsLoader } from '@astrojs/starlight/loaders';
-import { docsSchema } from '@astrojs/starlight/schema'
+import { defineCollection } from 'astro:content';
+import { docsLoader, i18nLoader } from '@astrojs/starlight/loaders';
+import { docsSchema, i18nSchema } from '@astrojs/starlight/schema';
+import { docSearchI18nSchema } from 'starlight-docsearch-typesense/schema';
 
 export const collections = {
   docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-}
+  i18n: defineCollection({
+    loader: i18nLoader(),
+    schema: i18nSchema({ extend: docSearchI18nSchema() }),
+  }),
+};
