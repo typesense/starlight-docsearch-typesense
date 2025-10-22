fc01a7e1fc99e6bebd88cf9c423dd94b836a572b
Hayden Hung Hoang <hayden.hhoang@gmail.com>
2025-10-22 09:46:50 +0700

    add .env.example

--- docs/src/config/docsearch.ts
+++ docs/src/config/docsearch.ts
@@ -1,10 +1,16 @@
 import type { DocSearchClientOptions } from 'starlight-docsearch-typesense';
 
 export default {
-  typesenseCollectionName: 'starlight_typesense_docs',
+  typesenseCollectionName: 'starlight_docsearch_typesense_docs',
   typesenseServerConfig: {
-    nodes: [{ url: 'http://localhost:8108' }],
-    apiKey: 'xyz',
+    nodes: [
+      {
+        host: import.meta.env.PUBLIC_TYPESENSE_HOST || 'localhost',
+        port: import.meta.env.PUBLIC_TYPESENSE_PORT || 8108,
+        protocol: import.meta.env.PUBLIC_TYPESENSE_PROTOCOL || 'http',
+      },
+    ],
+    apiKey: import.meta.env.PUBLIC_TYPESENSE_SEARCH_ONLY_API_KEY || 'xyz',
   },
   getMissingResultsUrl({ query }) {
     return `https://github.com/typesense/typesense-docsearch.js/issues/new?title=${query}`;
